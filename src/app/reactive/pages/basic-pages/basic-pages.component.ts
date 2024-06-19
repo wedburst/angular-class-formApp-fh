e20dacc9172c9bb683f7b5915e58d54b65082411
wedburst <jhonrawed@gmail.com>
2024-06-18 22:51:00 -0500

    feat: mejoras en basic y agregando dynamic valildator

--- src/app/reactive/pages/basic-pages/basic-pages.component.ts
+++ src/app/reactive/pages/basic-pages/basic-pages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const rtx5090 = {
   name: 'RTX 5090',
@@ -7,22 +7,25 @@ const rtx5090 = {
   inStorage: 6,
 }
 
+
 @Component({
   templateUrl: './basic-pages.component.html',
-  styles: []
+  styles: [
+  ]
 })
 export class BasicPagesComponent implements OnInit {
-  //public myForm: FormGroup = new FormGroup({
-  //  name: new FormControl(''),
-  //  price: new FormControl(0),
-  //  inStorage: new FormControl(0),
-  //}); video 237
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', []),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.minLength(3) ] ],
     price: [0, [ Validators.required, Validators.min(0) ] ],
     inStorage: [0, [ Validators.required, Validators.min(0) ]],
-  });
+  })
 
   constructor( private fb: FormBuilder ){}
 
@@ -54,12 +57,18 @@ export class BasicPagesComponent implements OnInit {
     return null;
   }
 
-  onSave():void{
-    console.log("Form:", this.myForm.value);
+
+  onSave():void {
+
     if ( this.myForm.invalid ) {
       this.myForm.markAllAsTouched();
       return;
     }
+
+    console.log(this.myForm.value);
+
     this.myForm.reset({ price: 0, inStorage: 0 });
+
   }
+
 }
